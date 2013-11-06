a59207fe84429208b234d4711bbcb6da7dbf9387
Maori Ito <maori@nibio.go.jp>
2013-11-06 14:17:39 +0900

    Update urlrdfa.php

--- urlrdfa.php
+++ urlrdfa.php
@@ -2,7 +2,19 @@
 include_once("RDFaLite.php");
 function parse_item(&$item){
 //    print_r($item);//." aa \n";
+//prefixの順番毎に出力。奇数だったら中身，偶数だったら種類。
     $type = $item->type[0];
+    $i = 0;
+    foreach($item->prefix as $prefix){
+       if($i%2 == 0){ 
+           $prefix = str_replace(":","=",$prefix);
+        print "@prefix_".$prefix;
+       }
+       else{
+        print $prefix."\n";
+       }
+        $i += 1;
+    }
 //    $type = substr(strrchr($type,"/"),1);
    foreach($item->properties as $property => $values){
         if($property == "taxon" or $property == "seeAlso"){//taxonまたはseeAlsoの場合にはNestに配慮
@@ -11,7 +23,13 @@ function parse_item(&$item){
                if(is_object($value)){//Nest or not
                    parse_item2($value,$nestproperty);
                  }else{
+                 //:が含まれていたプロパティは，外部のボキャブラリ使用と判断。
+                     if(strstr($property,":")){
+                         $replaceproperty = str_replace(":","_",$property);
+                    print "@".$replaceproperty."=".$value."\n";
+                     }else{
                     print "@".$type."_".$property."=".$value."\n";
+                     }
                  }
            }
         }
@@ -20,7 +38,13 @@ function parse_item(&$item){
                if(is_object($value)){//Nest or not
                    parse_item($value);
                  }else{
+                //:が含まれていたプロパティは，外部のボキャブラリ使用と判断。
+                     if(strstr($property,":")){
+                         $replaceproperty = str_replace(":","_",$property);
+                    print "@".$replaceproperty."=".$value."\n";
+                     }else{
                     print "@".$type."_".$property."=".$value."\n";
+                     }
         }
    } 
 }
